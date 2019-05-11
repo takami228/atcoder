8d5fdca5f6d536c0a6be1b841f1ed18047989d7e
takami228 <m.takami228@gmail.com>
2019-05-12 00:33:35 +0900

    update

--- diverta2019/c.cpp
+++ diverta2019/c.cpp
@@ -7,6 +7,7 @@ int main(){
     cin >> n;
     int end_a = 0;
     int start_b = 0;
+    int start_b_and_a = 0;
     int include_ab = 0;
     for(int i = 0; i < n; i++){
         string s;
@@ -18,8 +19,7 @@ int main(){
             end_a++;
         }
         if(s.at(0) == 'B' && s.at(s.size()-1) == 'A'){
-            start_b++;
-            end_a++;
+            start_b_and_a++;
         }
         for(int i = 0; i < s.size()-1; i++){
             if(s.at(i) == 'A' && s.at(i+1) == 'B'){
@@ -27,6 +27,12 @@ int main(){
             }
         }
     }
-    cout << include_ab + min(start_b, end_a) << endl;
+    int ans = include_ab;
+    if(start_b + end_a == 0 && start_b_and_a > 0){
+        ans += start_b_and_a - 1;
+    } else {
+        ans += start_b_and_a + min(start_b, end_a);
+    }
+    cout << ans << endl;
     return 0;
 }
