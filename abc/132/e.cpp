ac27405698537266d85a2c85a86a598c31ce7876
takami228 <m.takami228@gmail.com>
2019-06-30 14:36:35 +0900

    update

--- abc/132/e.cpp
+++ abc/132/e.cpp
@@ -14,29 +14,45 @@ int main(){
       cin >> u >> v;
       G[u].push_back(v);
     }
+
+    bool isVisited[n+1];
+    for(int i = 1; i <= n; i++){
+        isVisited[i] = false;
+    }
+
     int s, t;
     cin >> s >> t;
  
+    isVisited[s] = true;
     queue<pair<int, int> > queue;
     for(int i = 0; i < G[s].size(); i++){
         pair<int, int> p = make_pair(G[s][i], 1);
         queue.push(p);
     }
     int ans = -1;
-    int count = 0;
     while(!queue.empty()){
-        bool isEnd = false;
         pair<int, int> next = queue.front();
         int node = next.first;
         int depth = next.second;
         queue.pop();
-        if(node == t && depth % 3 == 0){
-            ans = depth / 3;
-            break;
+        if(depth % 3 == 0){
+            isVisited[node] = true;
+            if(node == t){
+                ans = depth / 3;
+                break;
+            }
         }
         for(int i = 0; i < G[node].size(); i++){
-            pair<int, int> p = make_pair(G[node][i], depth + 1);
-            queue.push(p);
+            int node_next = G[node][i];
+            int depth_next = depth + 1;
+            pair<int, int> p = make_pair(node_next, depth_next);
+            if(depth_next % 3 == 0){
+                if(!isVisited[node_next]){
+                    queue.push(p);
+                }
+            } else {
+                queue.push(p);
+            }
         }
         if(depth == 3*n + 1){
             ans = -1;
